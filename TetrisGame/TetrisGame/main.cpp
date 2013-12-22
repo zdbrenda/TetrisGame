d83f28857404dfd9eb9c559b4ed0143302df269b
zdbrenda <zdbrenda@gmail.com>
2013-12-22 12:42:33 -0500

    i wrote a comment

--- TetrisGame/TetrisGame/main.cpp
+++ TetrisGame/TetrisGame/main.cpp
@@ -7,7 +7,7 @@ int main()
     sf::CircleShape shape(100.f);
 	// so now im messing with the master
     shape.setFillColor(sf::Color::Green);
-
+	// this is a comment!
     while (window.isOpen())
     {
 		//handles loops
