6875b52a7706d4f18fef8f316bf7c82a118bf884
zdbrenda <zdbrenda@gmail.com>
2013-12-22 13:35:01 -0500

    made the circle move

--- TetrisGame/TetrisGame/main.cpp
+++ TetrisGame/TetrisGame/main.cpp
@@ -6,21 +6,52 @@ int main()
 	sf::RenderWindow window(sf::VideoMode(sf::VideoMode::getDesktopMode().width-20, sf::VideoMode::getDesktopMode().height-90), "SFML works!");
     sf::CircleShape shape(100.f);
 	// so now im messing with the master
-    shape.setFillColor(sf::Color::Green);
-	//hahaha, This is me.
+	shape.setFillColor(sf::Color::Magenta);
+	double x=0;
+	double y=0;
     while (window.isOpen())
     {
-		//handles loops
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+		
+		sf::Event event;
+		while( window.pollEvent(event)){
+			if(event.type== sf::Event::Closed){
+			window.close();}
 
+			if (event.key.code == sf::Keyboard::Up)
+    {  y-=50;
+       
+    }
+		if (event.key.code == sf::Keyboard::Down)
+    {
+       y+=50;
+    }
+		if (event.key.code == sf::Keyboard::Left)
+    {
+       x-=50;
+    }
+		if (event.key.code == sf::Keyboard::Right)
+    {
+       x+=50;
+    }
+		
+		
+		
+		}
+
+
+		shape.setPosition(x,y);
         window.clear();
         window.draw(shape);
         window.display();
+		x+=0.35;
+		if(x>=sf::VideoMode::getDesktopMode().width-20)
+			{x=0;
+		y+=100;}
+
+		if(y>=sf::VideoMode::getDesktopMode().height-90)
+			{y=0;
+		x=0;}
+		
     }
 
     return 0;
