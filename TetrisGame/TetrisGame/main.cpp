9e481daeab9a133308dc2c2b57fcc667adcb09ed
Samureye <daniel.caterson@gmail.com>
2013-12-22 00:36:23 -0500

    ya

--- TetrisGame/TetrisGame/main.cpp
+++ TetrisGame/TetrisGame/main.cpp
@@ -7,8 +7,9 @@ using namespace std;
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(sf::VideoMode::getDesktopMode().width-20, sf::VideoMode::getDesktopMode().height-90), "SFML works!");
-	//bacon for window
+
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
